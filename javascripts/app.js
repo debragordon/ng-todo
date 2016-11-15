5c1bfa9a3bfb7f7d96a1c296c5bb739cb6c79d27
Debra Gordon <debra.m.gordon@gmail.com>
2016-11-14 21:32:36 -0600

    resest the DOM to display with angular notation

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,5 +1,39 @@
 "use strict";
 
-console.log("inside app.js")
+var app = angular.module("TodoApp", []);
 
+app.controller("NavCtrl", function ($scope){
+  $scope.navItems = [{name:"Logout"}, {name:"All Items"}, {name:"New Items"}];
+});
 
+app.controller("TodoCtrl", function ($scope){
+  $scope.welcome = "hello";
+  $scope.showListView = true;
+  $scope.items = [
+    {
+      id: 0,
+      task: "mow the lawn",
+      isCompleted: true,
+      assignedTo: "Zoe"
+    },
+    {
+      id: 1,
+      task: "take a nap",
+      isCompleted: true,
+      assignedTo: "Zoe"
+    },
+    {
+      id: 2,
+      task: "Do The Laundry",
+      isCompleted: false,
+      assignedTo: "William"
+    }
+  ];
+
+  $scope.allItems = function(){
+  $scope.showListView = true;
+  }
+  $scope.newItem = function(){
+  $scope.showListView = false;
+  }
+});
