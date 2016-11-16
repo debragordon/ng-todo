5bb72f1b4e9c845f41bcc2a2b363c2ba8ff3cedb
Debra Gordon <debra.m.gordon@gmail.com>
2016-11-15 20:16:22 -0600

    connected to FB

---
+++ javascripts/controllers/TodoCtrl.js
@@ -0,0 +1,45 @@
+"use strict";
+
+app.controller("TodoCtrl", function ($scope){
+  $scope.welcome = "hello";
+  $scope.showListView = true;
+  $scope.newTask = {};
+  $scope.items = [
+    {
+      id: 0,
+      task: "mow the lawn",
+      isCompleted: true,
+      assignedTo: "Zoe"
+    },
+    {
+      id: 1,
+      task: "take a nap",
+      isCompleted: true,
+      assignedTo: "Zoe"
+    },
+    {
+      id: 2,
+      task: "Do The Laundry",
+      isCompleted: false,
+      assignedTo: "William"
+    }
+  ];
+
+  $scope.allItems = function(){
+    $scope.showListView = true;
+  }
+
+  $scope.newItem = function(){
+    $scope.showListView = false;
+  }
+
+  $scope.addNewItem = function(){
+    $scope.newTask.isCompleted = false;
+    $scope.newTask.id = $scope.items.length;
+    console.log("newTask in function", $scope.newTask);
+    $scope.items.push($scope.newTask);
+    $scope.newTask = {};
+    $scope.showListView = true;
+  }
+
+});
