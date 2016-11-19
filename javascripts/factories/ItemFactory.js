13ffde7eef9812b766d8f70b4620523909512b01
Debra Gordon <debra.m.gordon@gmail.com>
2016-11-19 13:59:04 -0600

    connected routing

--- javascripts/factories/ItemFactory.js
+++ javascripts/factories/ItemFactory.js
@@ -48,7 +48,35 @@ app.factory("ItemFactory", function($q, $http, FIREBASE_CONFIG) {
     });
   };
 
+  var getSingleItem = function(itemId){
+    return $q((resolve, reject)=>{
+      $http.get(`${FIREBASE_CONFIG.databaseURL}/items/${itemId}.json`)
+      .success(function(getSingleItemResponse){
+        resolve(getSingleItemResponse);
+      })
+      .error(function(getSingleItemError){
+        reject(getSingleItemError);
+      });
+    });
+  };
 
-  return {getItemList:getItemList, postNewItem:postNewItem, deleteItem:deleteItem};
+  var editItem = function(editItem){
+    return $q((resolve, reject)=>{
+      $http.put(`${FIREBASE_CONFIG.databaseURL}/items/${editItem.id}.json`, JSON.stringify({
+        assignedTo: editItem.assignedTo,
+        isCompleted: editItem.isCompleted,
+        task: editItem.task
+        })
+      )
+        .success(function(editResponse){
+          resolve(editResponse);
+        })
+        .error(function(editError){
+          reject(editError);
+        });
+    });
+  };
+
+  return {getItemList:getItemList, postNewItem:postNewItem, deleteItem:deleteItem, getSingleItem:getSingleItem, editItem:editItem};
 
 });
