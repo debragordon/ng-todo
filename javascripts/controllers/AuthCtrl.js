7809165a527ca563ee8607951cc7da97b256bd73
Debra Gordon <debra.m.gordon@gmail.com>
2016-11-22 20:53:57 -0600

    updating things

--- javascripts/controllers/AuthCtrl.js
+++ javascripts/controllers/AuthCtrl.js
@@ -4,6 +4,12 @@ app.controller("AuthCtrl", function($scope, $location, $rootScope, AuthFactory, 
   $scope.loginContainer = true;
   $scope.registerContainer = false;
 
+  if($location.path() === "/logout"){
+    AuthFactory.logout();
+    $rootScope.user = {};
+    $location.url("/auth");
+  }
+
   let logMeIn = function(logInStuff){
     AuthFactory.authenticate(logInStuff).then(function(didLogin){
       return UserFactory.getUser(didLogin.uid);
